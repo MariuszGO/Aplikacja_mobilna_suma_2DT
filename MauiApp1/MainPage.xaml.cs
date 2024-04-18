268c417c06c7dd0bd5f3287b066485e21f5d43f4
Pedagodzy <Pedagodzy@206SALA>
2024-04-18 10:16:34 +0200

    Dodaj pliki projektów.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+      
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Oblicz_Sume(object sender, EventArgs e)
+        {
+            int a, b=11, w;
+
+            if( Int32.TryParse(liczba_a.Text, out a) && Int32.TryParse(liczba_b.Text, out b))
+            {
+                a = Convert.ToInt32(liczba_a.Text);
+                b = Convert.ToInt32(liczba_b.Text);
+                w = a + b;
+
+                wynik.Text = w.ToString();
+
+            }
+
+            else
+            {
+                wynik.Text = "BŁĄD";
+
+            }
+
+           
+
+        }
+    }
+
+}
